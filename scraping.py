ebf12d8e7ad696bc064070a3e9a921afa2f3c24f
MischaKD <mkabdulov@gmail.com>
2021-06-11 18:57:26 +0300

    added two new requests

--- scraping.py
+++ scraping.py
@@ -8,3 +8,6 @@ html_data = BeautifulSoup(response.text)
 quotes = html_data.find_all(class_ = 'quote')
 for quote in quotes:
     print(quote.find(class_ = 'text').get_text())
+    print(quote.find(class_ = 'author').get_text())
+    print(quote.find(class_ = 'keywords')['content'])
+
